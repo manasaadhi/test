aaf4611b3d4e8a49b610b5b930422f3694c194ed
manasa <manasaadhi490@mirafra.com>
2019-11-14 15:38:24 +0530

    Bubble Sort

---
+++ bubble_sort.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#define N 5
+void Bubble_sort(int arr[],int );
+int main()
+{
+	int arr[N],i;
+	printf("Enter array elements:\n");
+	for(i=0;i<N;i++)
+		scanf("%d",&arr[i]);
+	printf("Before Sorting Array Elements are:\n");
+	for(i=0;i<N;i++)
+		printf("%d ",arr[i]);
+	printf("\n");
+	Bubble_sort(arr,N);
+
+} 
+void Bubble_sort(int a[],int n)
+{
+	int i,j,t;
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			if(a[j]>a[j+1])
+			{
+				t=a[j+1];
+				a[j+1]=a[j];
+				a[j]=t;
+			}
+		
+		}
+	}
+	printf("After sorting array elements are:\n");
+	for(i=0;i<n;i++)
+	printf("%d ",a[i]);
+	printf("\n\n");
+	
+}
